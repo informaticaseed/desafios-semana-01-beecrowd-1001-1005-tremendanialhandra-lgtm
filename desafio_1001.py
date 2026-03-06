3b1d28f163d9a036e7bdadd712cdd5c287a824d3
alhandratremendani <tremendanialhandra@gmail.com>
2026-03-06 11:54:04 -0300

    Implement addition of two integers in desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -8,7 +8,12 @@ Não apresente mensagem alguma além daquilo que está sendo especificado e
 não esqueça de imprimir o fim de linha após o resultado, caso contrário,
 você receberá "Presentation Error".
 """
+A = int(input())
+B = int(input())
 
+X = A + B
+
+print("X =", X)
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
 # Escreva sua solução abaixo
